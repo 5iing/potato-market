1393e76c639c87b5c7b326d9a27ec35c5ff4e9d1
leeyeonwoo <rexrex1105@gmail.com>
2025-09-23 10:27:15 +0900

    feat(page): add dummy chat page for make route items

---
+++ lib/pages/chat/chat_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ChatPage extends StatelessWidget {
+  const ChatPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('채팅'),
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      body: const Center(
+        child: Text('채팅 페이지'),
+      ),
+    );
+  }
+}
